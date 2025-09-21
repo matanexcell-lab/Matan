809f76ad6496ff48f31267a6db2eeec9b18ea60c
matanexcell-lab <matanexcell@gmail.com>
2025-09-22 01:18:47 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,31 @@
+let timer;
+let seconds = 0;
 
+function updateTimer() {
+    let hrs = String(Math.floor(seconds / 3600)).padStart(2, '0');
+    let mins = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
+    let secs = String(seconds % 60).padStart(2, '0');
+    document.getElementById("timer").innerText = `${hrs}:${mins}:${secs}`;
+}
+
+function startTimer() {
+    if (!timer) {
+        timer = setInterval(() => {
+            seconds++;
+            updateTimer();
+        }, 1000);
+    }
+}
+
+function stopTimer() {
+    clearInterval(timer);
+    timer = null;
+}
+
+function resetTimer() {
+    stopTimer();
+    seconds = 0;
+    updateTimer();
+}
+
+updateTimer();
